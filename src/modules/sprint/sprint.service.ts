14e427ead25cfa49251001dd602ab9a298bf69c5
Luka Bračun <luka@proxima.si>
2020-03-30 22:39:39 +0200

    product_owner can create new sprint

---
+++ src/modules/sprint/sprint.service.ts
@@ -0,0 +1,32 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LessThanOrEqual, MoreThanOrEqual, Repository } from 'typeorm';
+
+import { Sprint } from './sprint.entity';
+import { VSprint } from './sprint.validation';
+
+@Injectable()
+export class SprintService {
+  constructor(
+    @InjectRepository(Sprint) private sprintRepo: Repository<Sprint>,
+  ) {}
+
+  async createSprint(data: VSprint) {
+    await this.checkOverlaping(data);
+    return await this.sprintRepo.save(new Sprint(data));
+  }
+
+  private async checkOverlaping(data: VSprint) {
+    if (
+      await this.sprintRepo.findOne({
+        projectId: data.projectId,
+        endDate: MoreThanOrEqual(data.startDate),
+        startDate: LessThanOrEqual(data.endDate),
+      })
+    ) {
+      throw new ConflictException(
+        'Sprint overlaps with one or more existing sprints',
+      );
+    }
+  }
+}
---
+++ src/modules/sprint/sprint.validation.ts
@@ -0,0 +1,34 @@
+import { Transform } from 'class-transformer';
+import { Allow, IsDate, IsInt, IsNumber, Min } from 'class-validator';
+
+import { MinDateFun } from '../../utils/min-date.validator';
+import { Project } from '../project/project.entity';
+
+export class VSprint {
+  @IsNumber()
+  projectId: number;
+
+  // this is added by ptu-roles guard
+  @Allow()
+  project: Project;
+
+  @IsDate({
+    message: args =>
+      `${args.property} must be valid javascript string representation of Date`,
+  })
+  @MinDateFun(() => new Date())
+  @Transform(str => new Date(str))
+  startDate: Date;
+
+  @IsDate({
+    message: args =>
+      `${args.property} must be valid javascript string representation of Date`,
+  })
+  @MinDateFun(o => o.startDate)
+  @Transform(str => new Date(str))
+  endDate: Date;
+
+  @IsInt()
+  @Min(1)
+  velocity: number;
+}

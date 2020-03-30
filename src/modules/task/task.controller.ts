11dfc534d79b0aa2c4c947cc0bea60e3ea6c6b73
demianbucik <demian.bucik@gmail.com>
2020-03-30 23:53:05 +0200

    Task service and controller

---
+++ src/modules/task/task.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UseGuards,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { TaskService } from './task.service';
+import { Pagination } from '../../validators/pagination';
+import { DTask } from './task.dto';
+import { VTask, VTaskOpt } from './task.validation';
+import { OptionalParseIntPipe } from '../../validators/optional';
+import { EmptySearchPipe } from '../../validators/search';
+
+@Controller('task')
+@UseGuards(AuthGuard('jwt'))
+export class TaskController {
+  constructor(private taskService: TaskService) {}
+
+  @Get('all')
+  async listAll(
+    @Query() pagination: Pagination,
+    @Query('search', new EmptySearchPipe()) search: string,
+    @Query('user', new OptionalParseIntPipe()) user: number,
+    @Query('project', new OptionalParseIntPipe()) project: number,
+  ) {
+    console.log('User ' + user);
+    console.log('Project ' + project);
+    console.log('Search ' + search);
+    return (
+      await this.taskService.listAll(pagination, search, user, project)
+    ).map(task => new DTask(task));
+  }
+
+  @Post('create')
+  async createTask(@Body() data: VTask) {
+    return new DTask(await this.taskService.createTask(data));
+  }
+
+  @Put('update')
+  async updateTask(@Body() data: VTaskOpt) {
+    return new DTask(await this.taskService.updateTask(data));
+  }
+
+  @Delete('delete')
+  async deleteTask(@Body() data: VTaskOpt) {
+    return await this.taskService.deleteTask(data);
+  }
+}
---
+++ src/modules/task/task.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, Query } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
+import { FindOperator, Repository } from 'typeorm';
+import { VTask, VTaskOpt } from './task.validation';
+import { ILike } from '../../utils/ilike';
+import { Pagination } from '../../validators/pagination';
+
+@Injectable()
+export class TaskService {
+  constructor(@InjectRepository(Task) private taskRepo: Repository<Task>) {}
+
+  async findById(id: number) {
+    return await this.taskRepo.findOne(id);
+  }
+
+  async listAll(
+    { skip, take }: Pagination,
+    search: string,
+    userId: number,
+    projectId: number,
+  ) {
+    let where = [
+      { title: ILike(`%${search}%`) },
+      { description: ILike(`%${search}%`) },
+      { state: ILike(`%${search}%`) },
+    ];
+    let filter = {};
+    if (userId) {
+      filter = { ...filter, userId: new FindOperator('equal', userId) };
+    }
+    if (projectId) {
+      filter = { ...filter, projectId: new FindOperator('equal', projectId) };
+    }
+    where = where.map(elem => ({
+      ...elem,
+      ...filter,
+    }));
+    return await this.taskRepo.find({
+      order: { id: 'DESC' },
+      skip,
+      take,
+      where,
+    });
+  }
+
+  async createTask(data: VTask) {
+    return await this.taskRepo.save(new Task(data));
+  }
+
+  async updateTask(data: VTaskOpt) {
+    return await this.taskRepo.save(data);
+  }
+
+  async deleteTask(data: VTaskOpt) {
+    return await this.taskRepo.delete(data);
+  }
+}

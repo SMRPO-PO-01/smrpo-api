c472a6dce56c3e5907ef01043714d28f918a103d
demianbucik <demian.bucik@gmail.com>
2020-03-31 00:55:32 +0200

    Travis and eslint ignore

--- src/validators/optional.ts
+++ src/validators/optional.ts
@@ -1,7 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { PipeTransform } from '@nestjs/common/interfaces/features/pipe-transform.interface';
 
 @Injectable()
-export class OptionalParseIntPipe {
+export class OptionalParseIntPipe implements PipeTransform<string> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform(value, metadata) {
     if (value === null || value === undefined || value === '') {
       return null;
--- src/validators/search.ts
+++ src/validators/search.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PipeTransform } from '@nestjs/common/interfaces/features/pipe-transform.interface';
 
 @Injectable()
-export class EmptySearchPipe {
+export class EmptySearchPipe implements PipeTransform<string> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform(value, metadata) {
     return value || '';
   }

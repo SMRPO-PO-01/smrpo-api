66474bb352680583ae0eda047d7bcec25368cd94
Luka Bračun <luka@proxima.si>
2020-04-07 18:09:52 +0200

    fix failed merge

--- src/modules/project/project.validation.ts
+++ src/modules/project/project.validation.ts
@@ -1,16 +1,4 @@
-import { Type } from 'class-transformer';
-import { ArrayMinSize, IsArray, IsIn, IsInt, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
-
-import { ExactlyOneRole } from '../../utils/has-exactly-one.validator';
-import { PROJECT_USER_ROLE } from './project-user-role.enum';
-
-class VProjectUser {
-  @IsInt()
-  id: number;
-
-  @IsIn(Object.values(PROJECT_USER_ROLE))
-  role: PROJECT_USER_ROLE;
-}
+import { IsArray, IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class VProject {
   @IsString()

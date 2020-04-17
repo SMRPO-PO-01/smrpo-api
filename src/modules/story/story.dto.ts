11a3fe070d799a001fa2e01b69ca4444ca1abf6e
Luka Bračun <luka@proxima.si>
2020-04-17 15:36:13 +0200

    master or owner can create new stories in project

---
+++ src/modules/story/story.dto.ts
@@ -0,0 +1,20 @@
+import { STORY_PRIORITY } from './story-priority.enum';
+import { Story } from './story.entity';
+
+export class DStory {
+  id: number;
+  title: string;
+  description: string;
+  acceptanceTests: string;
+  priority: STORY_PRIORITY;
+  businessValue: number;
+
+  constructor(story: Story) {
+    this.id = story.id;
+    this.title = story.title;
+    this.description = story.description;
+    this.acceptanceTests = story.acceptanceTests;
+    this.priority = story.priority;
+    this.businessValue = story.businessValue;
+  }
+}

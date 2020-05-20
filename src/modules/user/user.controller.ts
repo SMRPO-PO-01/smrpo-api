b28c0e7487357b877505ed3d20de74627f71cb39
Luka Bračun <luka@proxima.si>
2020-05-20 13:25:13 +0200

    enable selfEdit (#30)

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,18 +1,32 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 
 import { AuthUser } from './auth/jwt.strategy';
 import { DUser } from './user.dto';
 import { User } from './user.entity';
 import { UserService } from './user.service';
+import { VUser } from './user.validation';
 
 @Controller('user')
 @UseGuards(AuthGuard('jwt'))
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private jwtService: JwtService) {}
 
   @Get('me')
   async getMe(@AuthUser() user: User) {
     return new DUser(user);
+  }
+
+  @Put('me')
+  async updateMe(@AuthUser() { id }: User, @Body() data: VUser) {
+    const user = await this.userService.editUser(id, data);
+    return new DUser(
+      user,
+      this.jwtService.sign({
+        username: user.username,
+        password: user.password,
+      }),
+    );
   }
 }
